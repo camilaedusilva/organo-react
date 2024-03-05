bd491325cee0b78f00e443a6692c49d60ec0f45c
Camila Eduardo da Silva <40743462+camilaedusilva@users.noreply.github.com>
2024-03-05 19:28:09 -0300

    A3 - Interação com o usuário

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Form';
 
 function App() {
+
+  const [colaboradores, setColaboradores] = useState ([])
+
+  const addNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores , colaborador])
+    console.log(colaborador)
+
+  }
+
   return (
     <div className="App">
 
         <Banner />
-        <Formulario />
+        <Formulario aoColaboradorCadastrado={colaborador => addNovoColaboradorAdicionado(colaborador)}/>
 
     </div>
   );
--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -3,10 +3,18 @@ import './CampoTexto.css'
 const CampoTexto = (props) => {
     const placeholderModify = `${props.placeholder}...`
 
+    //let valor ='Camila Silva';
+
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
+    
+
     return(
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder={placeholderModify}/>
+            <input  value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModify}/>
         </div>
     )
    
